de9924cca3327616712c4c380059fca1008bb334
Angelica Lirio <angelicablirio@gmail.com>
2022-03-30 19:15:06 -0300

    Update validations

--- scripts/index.js
+++ scripts/index.js
@@ -9,9 +9,10 @@ const linkCriarContaRef = document.querySelector('a')
 const validaEmail = () => {
   if (inputEmailRef.checkValidity()) {
     inputMsgErroEmailRef.classList.remove('show')
+    return true;
   } else {
     inputMsgErroEmailRef.classList.add('show')
-
+    return false
   }
 }
 
@@ -19,24 +20,31 @@ const validaEmail = () => {
 const validaSenha = () => {
   if (inputSenhaRef.checkValidity()) {
     inputMsgErroSenhaRef.classList.remove('show')
+    return true;
   } else {
     inputMsgErroSenhaRef.classList.add('show')
-
+    return false
   }
+}
+
+//Valida todos os campos do formulário
+const validaForm = () => {
+  return validaEmail() &&
+  validaSenha()
 }
 
 //Habilita o botão acessar
 const habilitaBtnAcessar = () =>{
-  inputBtnAcessarRef.removeAttribute('disabled')
+  inputBtnAcessarRef.disabled = !validaForm();
 }
 
 //Acessar a página do para criar conta caso não tenha
 const acessarPagCriarConta = () => {
-  window.location.assign("./pages/signup.html")
+  window.location.assign("./pages/signup.html");
 }
 
-inputEmailRef.addEventListener('keyup', validaEmail);
-inputSenhaRef.addEventListener('keyup', validaSenha);
+inputEmailRef.addEventListener('keyup', habilitaBtnAcessar);
+inputSenhaRef.addEventListener('keyup', habilitaBtnAcessar);
 inputBtnAcessarRef.addEventListener('click', (e) => {
   e.preventDefault()
 });
--- scripts/signup.js
+++ scripts/signup.js
@@ -16,8 +16,10 @@ const linkLoginRef = document.querySelector('a')
 const validaNome = () => {
   if (inputNomeRef.checkValidity()) {
     inputMsgErroNomeRef.classList.remove('show');
+    return true;
   } else {
     inputMsgErroNomeRef.classList.add('show');
+    return false;
   }
 }
 
@@ -25,8 +27,10 @@ const validaNome = () => {
 const validaApelido = () => {
   if (inputApelidoRef.checkValidity()) {
     inputMsgErroApelidoRef.classList.remove('show');
+    return true;
   } else {
     inputMsgErroApelidoRef.classList.add('show');
+    return false;
   }
 }
 
@@ -34,8 +38,10 @@ const validaApelido = () => {
 const validaEmail = () => {
   if (inputEmailRef.checkValidity()) {
     inputMsgErroEmailRef.classList.remove('show');
+    return true;
   } else {
     inputMsgErroEmailRef.classList.add('show');
+    return false;
   }
 }
 
@@ -43,8 +49,10 @@ const validaEmail = () => {
 const validaSenha = () => {
   if (inputSenhaRef.checkValidity()) {
     inputMsgErroSenhaRef.classList.remove('show');
+    return true;
   } else {
     inputMsgErroSenhaRef.classList.add('show');
+    return false;
   }
 }
 
@@ -52,9 +60,26 @@ const validaSenha = () => {
 const confirmaSenha = () => {
   if(inputSenhaRef.value !== inputSenhaConfirmRef.value) {
     inputMsgErroConfirmSenhaRef.classList.add('show');
+    return false;
   } else {
     inputMsgErroConfirmSenhaRef.classList.remove('show');
+    return true;
   }
+}
+
+//Valida todos os campos do formulário
+const validaFormCriarConta = () => {
+  return validaNome() &&
+  validaApelido() &&
+  validaEmail() &&
+  validaSenha() &&
+  confirmaSenha()
+}
+
+//Habilita o botão de criar conta
+const habilitaBtnCriarConta = () => {
+  inputBtnCriarContaRef.disabled = !validaFormCriarConta();
+
 }
 
 //Acessar a página do login caso já tenha conta
@@ -62,19 +87,13 @@ const acessarPagLogin = () => {
   window.location.assign("../index.html")
 }
 
-//Habilita o botão de criar conta
-const habilitaBtnCriarConta = () => {
-  inputBtnCriarContaRef.removeAttribute('disabled');
-
-}
-
 
 //Chama as funções
-inputNomeRef.addEventListener('keyup', validaNome);
-inputApelidoRef.addEventListener('keyup', validaApelido);
-inputEmailRef.addEventListener('keyup', validaEmail);
+inputNomeRef.addEventListener('keyup', habilitaBtnCriarConta);
+inputApelidoRef.addEventListener('keyup', habilitaBtnCriarConta);
+inputEmailRef.addEventListener('keyup', habilitaBtnCriarConta);
 inputSenhaRef.addEventListener('keyup', validaSenha);
-inputSenhaConfirmRef.addEventListener('keyup', confirmaSenha);
+inputSenhaConfirmRef.addEventListener('keyup', habilitaBtnCriarConta);
 linkLoginRef.addEventListener('click', acessarPagLogin)
 inputBtnCriarContaRef.addEventListener('click', (e) => {
   e.preventDefault();
