73a32e30324e05ede807cf2c55c460a2ee582e67
Angelica Lirio <angelicablirio@gmail.com>
2022-04-07 10:27:06 -0300

    Update tarefas.js

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -4,6 +4,7 @@ const btnCadastrarTarefasRef = document.querySelector('#cadastrarTarefa');
 const inputNovaTarefaRef = document.querySelector('#novaTarefa');
 const containerTarefas = document.querySelector('.tarefas-pendentes');
 const skeletonRef = document.querySelector('#skeleton');
+const btnRemoverTarefaRef = document.querySelector('.bin-img');
 
 //Inserir o nome do úsuário na tela
 const mostraNomeUsuário = () =>{
@@ -48,6 +49,7 @@ const mostraTarefas = () =>{
           <div class="descricao">
             <p class="nome">${task.description}</p>
             <p class="timestamp">Criada em: ${task.createdAt}</p>
+            <img class="bin-img" src="../assets/bin.png" alt="Remover tarefa">
           </div>
         </li>
           `
@@ -67,34 +69,52 @@ const postNovaTarefa = () => {
   let requestConfig = {
     method: 'POST',
     body: JSON.stringify(tasksRegister),
-    headers: {   // foi necessario adicionar aqui a autorização do token
+    headers: {
       'Content-Type': 'application/json',
       Authorization: localStorage.getItem('token')
     }
   }
 
   fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', requestConfig)
-    .then(response =>{
+    .then(response => {
       response.json()
       .then(data =>{
+        console.log(data)
         containerTarefas.innerHTML += `      
         <li class="tarefa">
         <div class="not-done"></div>
         <div class="descricao">
           <p class="nome">${data.description}</p>
           <p class="timestamp">Criada em: ${date.toLocaleDateString()}</p>
+          <img class="bin-img"src="../assets/bin.png" alt="Remover tarefa">
         </div>
       </li>
         `
       })
     })
 }
+const removerTarefa = () => {
 
-mostraNomeUsuário()
-mostraTarefas()
-btnCadastrarTarefasRef.addEventListener('click', e => {
-  e.preventDefault()
-  postNovaTarefa()
-});
+  let requestConfig = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type':'application/json',
+      Authorization: localStorage.getItem('token')
+    }
+  }
+
+  fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, requestConfig) 
+    .then(response => {
+      response.json()
+    .then(data => {
+      console.log(data)
+    });
+ });
+}
+
+mostraNomeUsuário();
+mostraTarefas();
+btnCadastrarTarefasRef.addEventListener('click', postNovaTarefa);
+btnRemoverTarefaRef.addEventListener('click', removerTarefa);
 
 
